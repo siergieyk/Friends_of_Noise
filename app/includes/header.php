741934b851a985e21298d40620478d3e4fa93782
adslin <shihpei.ling@gmail.com>
2019-05-19 02:36:10 -0700

    merge html and php code in web and app view

---
+++ app/includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Friends of Noise</title>
+  <link href="../web/css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>     
+      <div class="top">
+        <h4>Already a Member? <a href="login.html">User Visitor</a></h4>
+          <div class="logo"> 
+            <img src="../web/images/FoNlogo.png"> 
+          </div>
+      </div>  
+
+      <nav class="main">
+        <ul>
+          <li><a href="../web/admin_dash.html">Admin</a></li>
+        <li class="separator">|</li>
+          <li><a href="../web/member.html">Members</a></li>
+        <li class="separator">|</li>
+          <li><a href="../web/single-event.html">Events</a></li>
+        <li class="separator">|</li>
+          <li><a href="../web/login.html">Sign In</a></li>
+        <li class="separator">|</li>
+          <li><a href="../web/logout.html">Logout</a></li>
+        </ul>
+      </nav>   
