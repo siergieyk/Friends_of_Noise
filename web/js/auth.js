15af86da130db204c9aeb3f6afe924c859028f33
adslin <shihpei.ling@gmail.com>
2019-06-07 14:30:59 -0700

    Create auth.js

---
+++ web/js/auth.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyC0N7dTxzX551I3SeSMcxkx1PJbaAHzVR0",
+    authDomain: "fonproject-faf34.firebaseapp.com",
+    databaseURL: "https://fonproject-faf34.firebaseio.com",
+    projectId: "fonproject-faf34",
+    storageBucket: "fonproject-faf34.appspot.com",
+    messagingSenderId: "572276368012"
+};
+
+firebase.initializeApp(config);
