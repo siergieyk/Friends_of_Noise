681530a5bd03d800060257b62a4a85bb208ad77d
adslin <shihpei.ling@gmail.com>
2019-06-10 22:22:18 -0700

    fixed missing nav in header.php

--- app/includes/header.php
+++ app/includes/header.php
@@ -4,15 +4,29 @@
   <meta charset="UTF-8">
   <title>Friends of Noise</title>
   <link href="../web/css/style.css" rel="stylesheet" type="text/css">
+  <link rel="shortcut icon" href="../web/images/fon-yellow.jpg" /> 
 </head>
 
-<body>
+<body>     
       <div class="top">
         <h4>Already a Member? <a href="login.html">User Visitor</a></h4>
-          <div class="logo">
-            <img src="../web/images/FoNlogo.png">
+          <div class="logo"> 
+            <img src="../web/images/FoNlogo.png"> 
           </div>
-      </div>
+      </div>  
 
       <nav class="main">
         <ul>
+          <li><a href="http://www.friendsofnoise.org/" target="_blank">Home</a></li>
+        <li class="separator">|</li>
+          <li><a href="../web/event.html">Events</a></li>
+        <li class="separator">|</li>
+          <li><a href="../web/register.html">Sign Up</a></li>
+        <li class="separator">|</li>
+          <li><a href="../web/login.html">Sign In</a></li>
+        <div id="users" class="logout"> 
+        <li class="separator">|</li>
+          <li><a href="../web/logout.html" onclick="logout()">Logout</a></li>
+        </div>
+        </ul>
+      </nav>    
