a7f4fa51ea66948eb0c9c627a2ee02dac56c9474
adslin <shihpei.ling@gmail.com>
2019-06-11 21:54:58 -0700

    add sliding backgrounds

--- web/js/slide.js
+++ web/js/slide.js
@@ -6,13 +6,10 @@ $(document).ready(function() {
 
 function slideShow(speed) {
 
-  $('ul.slideshow').append('<li id="slideshow" class="slideshow"><div class="slideshow</div></li>');
+  $('ul.slideshow').append('<li class="slideshow"></li>');
 
-  $('ul.slideshow li').css({opacity: 0.0});
-  
+  $('ul.slideshow li').css({opacity: 0.0});  
 
-  $('ul.slideshow li:first').css({opacity: 1.0}).addClass('show');
-  
   $('#slideshow').html($('ul.slideshow li.show').find('img').attr('alt'));    
    
   var time = setInterval('gallery()', speed);
@@ -35,12 +32,10 @@ function gallery() {
 
   if(current.queue('fx').length == 0) {
 
-
     var next = ((current.next().length) ? ((current.next().attr('id') == 'slideshow')? $('ul.slideshow li:first') :current.next()) : $('ul.slideshow li:first'));
 
     var desc = next.find('img').attr('alt');  
  
-
     next.css({opacity: 0.0}).addClass('show').animate({opacity: 1.0}, 1000);
  
     current.animate({opacity: 0.0}, 1000).removeClass('show');
